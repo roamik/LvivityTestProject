ab1c5a5f2fb92f87f12848bd0015a9ecd397063b
roamik <roam.ivaniv@gmail.com>
2018-02-22 13:48:37 +0200

    Created project

---
+++ MyTestProject/MyTestProject/Models/AppConnectionStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTestProject.Models
+{
+    public class AppConnectionStrings
+    {
+        public string DatabaseContext { get; set; }
+    }
+}
