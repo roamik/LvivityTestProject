5707128d5eb3367c5dc48a4dc73021d016349b76
roamik <roam.ivaniv@gmail.com>
2018-02-23 12:13:26 +0200

    Modified AccountController, minor fixes

--- MyTestProject/MyTestProject/Controllers/AccountController.cs
+++ MyTestProject/MyTestProject/Controllers/AccountController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using MyTestProject.Helpers;
 using MyTestProject.Models;
-using Newtonsoft.Json;
+using MyTestProject.Options;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -13,69 +14,77 @@ using System.Threading.Tasks;
 
 namespace MyTestProject.Controllers
 {
+    [Route("api/[controller]")]
+    [AllowAnonymous]
     public class AccountController : Controller
     {
         private readonly DatabaseContext _context;
+        private readonly IOptions<JWTOptions> _options;
 
-        public AccountController(DatabaseContext context)
+        public AccountController(DatabaseContext context, IOptions<JWTOptions> options)
         {
             _context = context;
+            _options = options;
         }
 
-        [HttpPost("/login")]
-        public async Task Login()
-        {
-            var username = Request.Form["username"];
-            var password = Request.Form["password"];
-
-            var identity = GetIdentity(username, password);
-            if (identity == null)
-            {
-                Response.StatusCode = 400;
-                await Response.WriteAsync("Invalid username or password.");
-                return;
-            }
-
-            var now = DateTime.UtcNow;
-            // creating jwt token
-            var jwt = new JwtSecurityToken(
-                    issuer: AuthOptions.ISSUER,
-                    audience: AuthOptions.AUDIENCE,
-                    notBefore: now,
-                    claims: identity.Claims,
-                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
-                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
-            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
-
-            var response = new
-            {
-                access_token = encodedJwt,
-                username = identity.Name
-            };
-
-            // responce serialization
-            Response.ContentType = "application/json";
-            await Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
-        }
-
-        private ClaimsIdentity GetIdentity(string username, string password)
+        // POST api/account/login
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(string username, string password)
         {
             var user = _context.Users.First(x => x.Login == username && x.Password == password);
-            if (user != null)
+            if (user == null)
             {
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login),
-                    new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role)
+                ModelState.AddModelError("Email", "Not found");
+            }
+            else
+            {
+                var claims = new List<Claim> {
+                    new Claim( ClaimsIdentity.DefaultNameClaimType, user.Login ),
+                    new Claim( JwtRegisteredClaimNames.Sub, user.Login ),
+                    new Claim( JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString() )
                 };
-                ClaimsIdentity claimsIdentity =
-                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
-                    ClaimsIdentity.DefaultRoleClaimType);
-                return claimsIdentity;
+                var creds = new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: _options.Value.Issuer,
+                    audience: _options.Value.Issuer,
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token), id = user.Id });
             }
 
-            // if there is no user
-            return null;
+            return BadRequest(ModelState);
         }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(string username, string password, string role)
+        {
+
+            var user = new User { Login = username, Password = password, Role = role };
+            _context.Add(user);
+            _context.SaveChanges();
+            var claims = new[]
+            {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType,user.Login),
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Login),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+            var creds = new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_options.Value.Issuer,
+                _options.Value.Issuer,
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token), id = user.Id });
+        }
+
     }
+
 }
---
+++ MyTestProject/MyTestProject/Options/JWTOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTestProject.Options
+{
+    public class JWTOptions
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+    }
+}
